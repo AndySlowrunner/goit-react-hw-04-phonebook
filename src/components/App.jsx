2a5b8b98c73346117b820771b2e7a0cdc41898bc
Andy <andyfartushniy@gmail.com>
2023-10-05 18:49:22 +0300

    Updated task 4.2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,84 +1,69 @@
-import { Component } from "react";
 import { StyledDiv } from "./ContactForm/StyledForm";
 import { ContactForm } from "./ContactForm/ContactForm";
 import { ContactList } from "./ContactList/ContactList";
 import { Filter } from "./ContactFilter/ContactFilter";
+import { useEffect, useState } from "react";
 
-export class App extends Component {   
-    state = {
-        contacts: [
-            {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-            {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-            {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-            {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-        ],
-        filters: '',
-    };
+export const App = () => {
+    const [contacts, setContacts] = useState([
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ]);
+
+    const [filters, setFilters] = useState('');
     
-    componentDidMount() {
-        const contacts = localStorage.getItem('contacts');
-        const parsContacts = JSON.parse(contacts);
-
-        if (parsContacts) {
-            this.setState({contacts: parsContacts})
+    useEffect(() => {
+        localStorage.setItem('contacts', JSON.stringify(contacts))
+    }, [contacts]);
+    
+    
+    useEffect(() => {
+        const users = localStorage.getItem('contacts');
+        if (users) {
+            const parsContacts = JSON.parse(users);
+            setContacts(parsContacts)
         }
+    }, []);
+
+    
+    const handleDelete = (userId) => {
+        setContacts(prevState => prevState.filter(contact => contact.id !== userId)
+        )
     };
 
-    componentDidUpdate(prevProps, prevState) {
-        if (this.state.contacts !== prevState.contacts){
-            localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
-        }
+    const changeTopicFilter = (newTopic) => {
+        setFilters(newTopic);
     };
 
-    addContact = newContact => {
-        const isExist = this.state.contacts.some(contact => contact.name === newContact.name);
+    const addContact = (newContact) => {
+        const isExist = contacts.some(contact => contact.name === newContact.name);
         
         if (isExist) {
             alert(`${newContact.name} is already in contacts.`);
             return
         };
         
-        this.setState(prevState => {
-            return {
-                contacts: [...prevState.contacts, newContact]
-            }
-        });
+        setContacts(prevState => [...prevState, newContact]);
     };
 
-    changeTopicFilter = (newTopic) => {
-        this.setState({
-            filters: newTopic
-        });
-    };
-
-    handleDelete = (userId) => {
-        this.setState(prevState => {
-            return {
-                contacts: prevState.contacts.filter(contact => contact.id !== userId),
-            };
-        });
-    };
-
-    render() {
-        const { contacts, filters } = this.state;
-
-        const visibleContacts = contacts.filter(contact =>
-            contact.name.toLowerCase().includes(filters.toLowerCase()));
-        
-        return (
-            <StyledDiv>
-                <h1>Phonebook</h1>
-                <ContactForm onAdd={this.addContact} />
-                <h2>Contacts</h2>
-                <Filter
-                    value={filters}
-                    onChange={this.changeTopicFilter}
-                />
-                <ContactList
-                    contacts={visibleContacts}
-                    onClick={this.handleDelete}
-                />
-            </StyledDiv>
-        );
-    };
-}
+    const visibleContacts = contacts.filter(contact =>
+        contact.name.toLowerCase().includes(filters.toLowerCase()));
+    
+    return (
+        <StyledDiv>
+            <h1>Phonebook</h1>
+            <ContactForm onAdd={addContact} />
+            <h2>Contacts</h2>
+            <Filter
+                value={filters}
+                onChange={changeTopicFilter}
+            />
+            <ContactList
+                contacts={visibleContacts}
+                onClick={handleDelete}
+            />
+        </StyledDiv>
+    );
+ };
